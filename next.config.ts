465c02d354c4e3b8da8d1cc55029d66b3fc3765b
Brennen-Beck <brennen_beck@yahoo.com>
2025-02-13 22:48:45 -0600

    Tried to turn on caching.

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,18 @@ const nextConfig: NextConfig = {
   cache: {
     enabled: true, // Enabling cache for the build
   },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Enable caching for client-side builds
+      config.cache = {
+        type: 'filesystem', // This will cache the build files on disk
+        buildDependencies: {
+          config: [__filename], // Ensure that the cache is invalidated when the config changes
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
